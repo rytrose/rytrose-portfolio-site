a7d68e2ff24424cbf55fb26ac76b83fe0afc6593
Ryan Rose <rytrose@gmail.com>
2022-09-08 09:52:08 -0400

    add responsive canvas

--- components/graffiti/GraffitiCanvas.js
+++ components/graffiti/GraffitiCanvas.js
@@ -1,20 +1,90 @@
-import { useEffect } from "react";
+import { fabric } from "fabric";
+import { useEffect, useRef, useCallback } from "react";
+import useResizeObserver from "use-resize-observer";
 import useFabric from "../../hooks/useFabric";
 
 const GraffitiCanvas = () => {
-  const { canvasElRef, fabricCanvasRef } = useFabric();
+  const { canvasElRef, fabricCanvasRef } = useFabric({
+    selection: false,
+  });
+  const divRef = useRef();
 
+  // onDivResize resizes the canvas responsively
+  const onDivResize = useCallback(
+    (size) => {
+      if (!size) return;
+      if (!fabricCanvasRef.current) return;
+      const canvas = fabricCanvasRef.current;
+
+      const ratio = canvas.getWidth() / canvas.getHeight();
+      const containerWidth = size.width;
+      const containerHeight = size.height;
+      const maxWidthHeight = containerWidth / ratio;
+      let scale, zoom, width, height;
+
+      // If setting the height to max width would make it taller than the container,
+      // use the container height instead to prevent scrolling.
+      if (maxWidthHeight > containerHeight) {
+        scale = containerHeight / canvas.getHeight();
+        zoom = canvas.getZoom() * scale;
+        width = containerHeight * ratio;
+        height = containerHeight;
+      } else {
+        // Set the canvas to max width
+        scale = containerWidth / canvas.getWidth();
+        zoom = canvas.getZoom() * scale;
+        width = containerWidth;
+        height = containerWidth / ratio;
+      }
+      canvas.setDimensions({
+        width: width,
+        height: height,
+      });
+      canvas.setViewportTransform([zoom, 0, 0, zoom, 0, 0]);
+    },
+    [fabricCanvasRef]
+  );
+
+  useResizeObserver({
+    ref: divRef,
+    onResize: onDivResize,
+  });
+
+  // Sets up all things for the canvas
+  // TODO: refactor into custom hook
   useEffect(() => {
     if (!fabricCanvasRef.current) return;
     const canvas = fabricCanvasRef.current;
-    canvas.on("mouse:over", () => {
-      console.log("MOUSEOVER");
+    const circle = new fabric.Circle({
+      originX: "center",
+      originY: "center",
+      radius: 50,
+      fill: "green",
+      selectable: false,
+      hoverCursor: "auto",
     });
+    canvas.add(circle);
+
+    /*
+      TODO:
+        - On touchdown/clickdown create Group size == canvas
+        - On drage, use canvas position to spawn circles/shapes "paint", add to Group
+        - Subtract area of painted shapes from user's available "paint"
+        - Stop adding when touchup or user is out of "paint"
+    */
   }, [fabricCanvasRef]);
 
   return (
-    <div>
-      <canvas ref={canvasElRef} width={500} height={500} />
+    <div
+      ref={divRef}
+      className="flex justify-center sm:h-[100vh] sm:max-h-[calc(100vh-162px)]"
+    >
+      <canvas
+        className="border border-slate-200"
+        ref={canvasElRef}
+        width={500}
+        height={500}
+      />
     </div>
   );
 };
--- hooks/useFabric.js
+++ hooks/useFabric.js
@@ -1,21 +1,25 @@
 import { fabric } from "fabric";
 import { useRef, useCallback } from "react";
 
-const useFabric = () => {
+const useFabric = (options) => {
+  console.log("use fabric");
   const canvasRef = useRef();
   const disposeRef = useRef();
   return {
-    canvasElRef: useCallback((node) => {
-      if (node) {
-        canvasRef.current = new fabric.Canvas(node);
-      } else if (canvasRef.current) {
-        canvasRef.current.dispose();
-        if (disposeRef.current) {
-          disposeRef.current();
-          disposeRef.current = undefined;
+    canvasElRef: useCallback(
+      (node) => {
+        if (node) {
+          canvasRef.current = new fabric.Canvas(node, options);
+        } else if (canvasRef.current) {
+          canvasRef.current.dispose();
+          if (disposeRef.current) {
+            disposeRef.current();
+            disposeRef.current = undefined;
+          }
         }
-      }
-    }, []),
+      },
+      [options]
+    ),
     fabricCanvasRef: canvasRef,
   };
 };
